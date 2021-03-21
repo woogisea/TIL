4212c2a26d76d294b30425ab247d4ffae45ffd67
Jaewook <rgn95652@gmail.com>
2021-03-21 12:16:53 +0900

    add : Sum

---
+++ Data Structures/Recursion/Sum.c
@@ -0,0 +1,26 @@
+//재귀의 작동 원리를 가장 기본적인
+//방법으로 이해
+
+//1~N까지의 합 구하기
+
+
+//#include <stdio.h>
+//int sum(n)
+//{
+//    if(n == 1)
+//    {
+//        return 1;
+//    }
+//    else
+//    {
+//        return n+sum(n-1);
+//    }
+//}
+//int main()
+//{
+//    int N;
+//    scanf("%d",&N);
+//
+//    printf("%d",sum(N));
+//}   //(1)
+
