ae82c201927d84d75805dd1b24bbc175a34f5075
Jaewook <rgn95652@gmail.com>
2021-03-21 12:20:12 +0900

    add : PrintDigits

---
+++ Data Structures/Recursion/code/PrintDigits.c
@@ -0,0 +1,24 @@
+//재귀의 작동 원리를 가장 기본적인
+//방법으로 이해
+
+//자릿수 하나하나 출력
+
+//#include <stdio.h>
+//void nPrintDigits(n)
+//{
+//    if(n < 10)
+//    {
+//        printf("%d\n",n);
+//    }
+//    else
+//    {
+//        nPrintDigits(n/10);
+//        printf("%d\n",n%10);
+//    }
+//}
+//int main()
+//{
+//    int n;
+//    scanf("%d",&n);
+//    nPrintDigits(n);
+//}
