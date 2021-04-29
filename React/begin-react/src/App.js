c73b7cf047a18b5f1685d85be0a6aa470e91d972
Jaewook <rgn95652@gmail.com>
2021-04-29 22:52:34 +0900

    React_src

---
+++ React/begin-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+import InputSameple from './InputSample';
+
+function App() {
+  return (
+    <InputSameple />
+  );
+}
+
+export default App;
