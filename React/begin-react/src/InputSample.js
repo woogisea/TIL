baf484b2f764126526ca0ddc1f7a2159bdf85657
Jaewook <rgn95652@gmail.com>
2021-05-07 22:59:43 +0900

    React : Hooks(useRef)

--- React/begin-react/src/InputSample.js
+++ React/begin-react/src/InputSample.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function InputSample() {
     const [text, setText] = useState(' ');
@@ -6,8 +6,10 @@ function InputSample() {
         name : ' ',
         nickname : ' '
     });
+    const nameInput = useRef();       //Ref 객체 생성
     
     const {name, nickname} = inputs;
+
 
     //input 상태를 한개만 관리 할 때
     const onChange = (e) => {
@@ -32,13 +34,14 @@ function InputSample() {
             name : ' ',
             nickname : ' '
         })
+        nameInput.current.focus();     //current => 현재 지정된 DOM
     }
 
     return (
         <div>
             <input onChange = {onChange} value = {text}/>                                                   {/*input 상태를 관리 할때에는 value 값도 설정하는 것이 중요하다 */}
-            <input name = "name" onChange = {onChange1} value = {name} placeholder = "이름" />
-            <input name = "nickname" onChange = {onChange1} value = {nickname} placeholder = "닉네임" />
+            <input name = "name" onChange = {onChange1} value = {name} ref = {nameInput} placeholder = "이름" />      {/*내가 원하는 부분에 ref값 설정  reset버튼을 클릭 후 focus가 name input으로 설정된다*/}
+            <input name = "nickname" onChange = {onChange1} value = {nickname}  placeholder = "닉네임" />
             <button onClick = {onReset}>초기화</button>
             <button onClick = {onReset1}>초기화1</button>
             <div>
