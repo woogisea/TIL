c65bf15b5401b56e5757b3a9cc316148b05f5ed6
Jaewook <rgn95652@gmail.com>
2021-05-08 23:33:15 +0900

    React : array(추가, 수정, 삭제)

--- React/begin-react/src/UserList.js
+++ React/begin-react/src/UserList.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 //컴포넌트 재사용
 //map 활용
-function User ({user}) {
+function User ({user, onRemove, onToggle}) {
     return (
         <div>
-            <b>{user.username}</b><span>({user.email})</span>
+            <b style = {{cursor : 'pointer', color : user.active ? 'green' : 'black'}} onClick = {() => onToggle(user.id)}>{user.username}</b><span>({user.email})</span>
+            <button onClick = {() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
-function UserList({users}) {
+function UserList({users, onRemove, onToggle}) {
     return(
         <div>
             {/*배열 렌더링 할때는 고유key값이 필요하다*/}
            {users.map(user  => (
-               <User user = {user} key = {user.id}/>
+               <User user = {user} key = {user.id} onRemove = {onRemove} onToggle = {onToggle} />
            ))}
         </div>
     );
