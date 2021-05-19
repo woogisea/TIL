15309ee19570e4ea29621555356f30bc14b50ab5
Jaewook <rgn95652@gmail.com>
2021-05-19 23:28:43 +0900

    React : TodoList

--- React/mashup-todolist/src/App.js
+++ React/mashup-todolist/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
-import TodoCreate from './components/TodoCreate';
+import TodoTemplate from './components/TodoTemplate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
-import TodoTemplate from './components/TodoTemplate';
+import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './TodoContext';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -13,14 +14,14 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
