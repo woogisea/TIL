fcfd8ba78a70bfa60e7629a75c5770505dac0a05
Jaewook <rgn95652@gmail.com>
2021-07-08 14:00:52 +0900

    modified

---
+++ React/hooks-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Average from './Average';
+import Counter from './Counter';
+import Info from './Info';
+
+const App = () => {
+  return (
+    <>
+      <Counter /> <br />
+      <Info /> <br />
+      <Average />
+    </> 
+  );
+};
+
+export default App;
