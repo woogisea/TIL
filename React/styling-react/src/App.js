cfb3c75d8de8bc41c5948809ac826a59696b02be
Jaewook <rgn95652@gmail.com>
2021-07-09 23:24:42 +0900

    modified

---
+++ React/styling-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CSSModule from './CSSModule';
+import SassComponent from './SassComponent';
+
+const App = () => {
+  return (
+    <div>
+      <SassComponent />
+      <br />
+      <CSSModule />
+    </div>
+  );
+};
+
+export default App;
