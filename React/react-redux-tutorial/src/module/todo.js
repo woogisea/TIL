3f867e43222fb46b0729a431349928332b2c2229
Jaewook <rgn95652@gmail.com>
2021-07-26 21:17:16 +0900

    update

--- React/react-redux-tutorial/src/module/todo.js
+++ React/react-redux-tutorial/src/module/todo.js
@@ -1,40 +1,55 @@
+import { createAction, handleActions } from 'redux-actions'; 
 const CHANGE_INPUT = 'todo/CHANGE_INPUT';
 const INSERT = 'todo/INSERT';
 const TOGGLE = 'todo/TOGGLE';
 const DELETE = 'todo/DELETE';
 
-export const changeInput = input => (
-    {
-        type : CHANGE_INPUT,
-        input
-    }
-);
+// export const changeInput = input => (
+//     {
+//         type : CHANGE_INPUT,
+//         input
+//     }
+// );
 
+// let nextId = 3;
+// export const insert = text => (
+//     {
+//         type : INSERT,
+//         next : {
+//             id : nextId++,
+//             text,
+//             checked : false
+//         }
+//     }
+// );
+
+// export const toggle = id => (
+//     {
+//         type : TOGGLE,
+//         id
+//     }
+// );
+
+// export const Tdelete = id => (
+//     {
+//         type : DELETE,
+//         id
+//     }
+// );
+
+export const changeInput = createAction(CHANGE_INPUT, input => input);
 let nextId = 3;
-export const insert = text => (
+export const insert = createAction(INSERT, text => (
     {
-        type : INSERT,
-        next : {
-            id : nextId++,
-            text,
-            checked : false
-        }
+        id : nextId++,
+        text,
+        checked : false
     }
-);
+));
 
-export const toggle = id => (
-    {
-        type : TOGGLE,
-        id
-    }
-);
+export const toggle = createAction(TOGGLE, id => id);
+export const Tdelete = createAction(DELETE, id => id);
 
-export const Tdelete = id => (
-    {
-        type : DELETE,
-        id
-    }
-);
 
 const initialState = {
     input : ' ',
@@ -53,32 +68,57 @@ const initialState = {
     ]
 };
 
-function todo(state = initialState, action) {
-    switch (action.type) {
-        case CHANGE_INPUT:
-            return{
-                ...state,
-                input : action.input
-            };
-        case INSERT:
-            return{
-                ...state,
-                todos : state.todos.concat(action.next)
-            };
-        case TOGGLE:
-            return{
-                ...state,
-                todos : state.todos.map(todo => todo.id === action.id ? {...todo, checked : !todo.checked} : todo)
-            };
-        case DELETE:
-            return{
-                ...state,
-                todos : state.todos.filter(todo => todo.id !== action.id)
-            };
+// function todo(state = initialState, action) {
+//     switch (action.type) {
+//         case CHANGE_INPUT:
+//             return{
+//                 ...state,
+//                 input : action.input
+//             };
+//         case INSERT:
+//             return{
+//                 ...state,
+//                 todos : state.todos.concat(action.next)
+//             };
+//         case TOGGLE:
+//             return{
+//                 ...state,
+//                 todos : state.todos.map(todo => todo.id === action.id ? {...todo, checked : !todo.checked} : todo)
+//             };
+//         case DELETE:
+//             return{
+//                 ...state,
+//                 todos : state.todos.filter(todo => todo.id !== action.id)
+//             };
     
-        default:
-            return state;
-    }
-}
+//         default:
+//             return state;
+//     }
+// }
+
+const todo = handleActions(
+    {
+        [CHANGE_INPUT] : (state,{payload : input}) => ({
+            ...state,
+            input : input
+        }),
+
+        [INSERT] : (state,{payload : text}) => ({
+            ...state,
+            todos : state.todos.concat(text)
+        }),
+
+        [TOGGLE] : (state,{payload : id}) => ({
+            ...state,
+            todos : state.todos.map(todo => todo.id === id ? {...todo, checked : !todo.checked} : todo)
+        }),
+
+        [DELETE] : (state,{payload : id}) => ({
+            ...state,
+            todos : state.todos.filter(todo => todo.id !== id)
+        })
+    },
+    initialState
+)
 
 export default todo;
