79461209593460016f3700d59c45a51d8efd127c
Jaewook <rgn95652@gmail.com>
2021-07-27 21:17:19 +0900

    blog

---
+++ React/blog/blog-backend/src/api/posts/posts.crtl.js
@@ -0,0 +1,102 @@
+let postId = 1;
+
+const posts = [
+    {
+        id : 1,
+        text : '제목',
+        body : '내용'
+    },
+];
+
+//POST
+exports.write = ctx => {
+    const { title, body } = ctx.request.body;
+    postId += 1;
+    const post = {
+        id : postId++,
+        title,
+        body
+    };
+    posts.push(post);
+    ctx.body = post;
+};
+
+//GET
+exports.list = ctx => {
+    ctx.body = posts;
+};
+
+//GET:id
+exports.read = ctx => {
+    const { id } = ctx.params;
+
+    const post = posts.find(p => p.id.toString() === id);
+
+    if(!post) {
+        ctx.statue = 404;
+        ctx.body = {
+            message : '포스트가 존재하지 않습니다'
+        };
+        return;
+    }
+    ctx.body = post;
+};
+
+//DELETE:id
+exports.remove = ctx => {
+    const { id } = ctx.params;
+
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if(index === -1) {
+        ctx.statue = 404;
+        ctx.body = {
+            message : '포스트가 존재하지 않습니다'
+        };
+        return;
+    };
+
+    posts.slice(index,1);
+    ctx.statue = 204;
+};
+
+//PUT:id
+exports.replace = ctx => {
+    const { id } = ctx.params;
+
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if(index === -1) {
+        ctx.statue = 404;
+        ctx.body = {
+            message : '포스트가 존재하지 않습니다'
+        };
+        return;
+    };
+
+    posts[index] = {
+        id,
+        ...ctx.request.body
+    };
+
+    ctx.body = posts[index];
+}
+
+//PATCH:id
+exports.update = ctx => {
+    const { id } = ctx.params;
+
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if(index === -1) {
+        ctx.statue = 404;
+        ctx.body = {
+            message : '포스트가 존재하지 않습니다'
+        };
+        return;
+    };
+
+    posts[index] = {
+        ...posts[index],
+        ...ctx.request.body
+    };
+
+    ctx.body = posts[index];
+};
