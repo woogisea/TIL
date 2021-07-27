baacab8ee2feca44593e32ce527dc8562f139dec
Jaewook <rgn95652@gmail.com>
2021-07-27 21:18:24 +0900

    middleware

---
+++ React/learn-redux-middleware/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CounterContainer from './container/CounterContainer';
+import SampleContainer from './container/SampleContainer';
+
+const App = () => {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ React/learn-redux-middleware/src/container/CounterContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increaseAsync, decreaseAsync } from '../module/counter';
+
+const CounterContainer = ({ number, increaseAsync, decreaseAsync}) => {
+    return (
+        <Counter number = {number} onIncrease = {increaseAsync} onDecrease = {decreaseAsync} />
+    );
+};
+
+export default connect(
+    state => ({ number : state.counter}),
+    {
+        increaseAsync,
+        decreaseAsync
+    }
+)(CounterContainer);
