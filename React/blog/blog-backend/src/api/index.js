626df983d5e0a98c6be9efa14dc82ed698b47932
Jaewook <rgn95652@gmail.com>
2021-08-17 22:52:33 +0900

    backend

--- React/blog/blog-backend/src/api/index.js
+++ React/blog/blog-backend/src/api/index.js
@@ -5,6 +5,7 @@ import auth from './auth';
 const api = new Router();
 
 api.use('/posts', posts.routes());
-api.use('/auth',auth.routes());
+api.use('/auth', auth.routes());
 
+// 라우터를 내보냅니다.
 export default api;
